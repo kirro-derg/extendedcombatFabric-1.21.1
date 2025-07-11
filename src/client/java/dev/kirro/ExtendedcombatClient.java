b55c02690e292c76591bae40d09c81623502c770
kirro-derg <shaneguidry3407@gmail.com>
2025-07-10 23:06:22 -0500

    initial commit

---
+++ src/client/java/dev/kirro/ExtendedcombatClient.java
@@ -0,0 +1,10 @@
+package dev.kirro;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtendedcombatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
